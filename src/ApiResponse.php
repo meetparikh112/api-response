3f46921ab7d489e7965409091080fbf42972c92d
meetparikh <meetparikh71@gmail.com>
2019-09-09 23:06:07 +0530

    initial commit

---
+++ src/ApiResponse.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Meet\ApiResponse;
+
+class ApiResponse
+{
+    /**
+     * static property for error response
+     *
+     * @var array
+     */
+    public static $errorResponse = array();
+
+
+    /**
+     * static property for success response
+     *
+     * @var array
+     */
+    public static $successResponse = array();
+
+
+    /**
+     * static method for error response
+     *
+     * @param string $message
+     * @param string $status
+     * @param integer $code
+     * @param [type] $data
+     * @return void
+     */
+    public static function getError(
+        $message = 'you got an error.',
+        $status = 'error',
+        $code = 500,
+        $data = null
+    ) {
+        self::$errorResponse['error']['message'] = $message;
+        self::$errorResponse['error']['status'] = $status;
+        self::$errorResponse['error']['code'] = $code;
+        $data != null ? self::$errorResponse['error']['data'] = $data : null;
+        return self::$errorResponse;
+    }
+
+
+    /**
+     * static method to get success response
+     *
+     * @param [type] $data
+     * @param string $status
+     * @param string $message
+     * @param integer $code
+     * @return void
+     */
+    public static function getResult(
+        $data = null,
+        $status = 'Ok',
+        $message = 'you got what you needed.',
+        $code = 200
+    ) {
+        self::$successResponse['Success']['status'] = $status;
+        self::$successResponse['Success']['code'] = $code;
+        self::$successResponse['Success']['message'] = $message;
+        self::$successResponse['Success']['data'] = $data;
+        return self::$successResponse;
+    }
+}
